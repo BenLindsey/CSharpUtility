ad89463162bedc172cac72fb4f49f77249d50eb5
Ben Lindsey <ben.lindsey@red-gate.com>
2013-09-03 14:03:49 +0100

    Setup project and implemented permutations

---
+++ Source/LinqExtenstions/Combinations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExtenstions
+{
+    public static class Combinations
+    {
+        public static IEnumerable<IEnumerable<T>> Permutations<T>(this IEnumerable<T> source)
+        {
+            var sourceArray = source as T[] ?? source.ToArray();
+
+            var count = sourceArray.Count();
+
+            if (count == 1)
+                yield return sourceArray;
+            else
+                for (var i = 0; i < count; i++)
+                    foreach (var p in Permutations(sourceArray.Take(i).Concat(sourceArray.Skip(i + 1))))
+                        yield return sourceArray.Skip(i).Take(1).Concat(p);
+        }
+    }
+}
---
+++ Tests/LinqExtensionsTests/CombinationsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqExtenstions;
+using NUnit.Framework;
+
+namespace LinqExtensionsTests
+{
+    [TestFixture]
+    public class CombinationsTests
+    {
+        public static IEnumerable<TestCaseData> IntEnumerables()
+        {
+            return new[]
+            {
+                new TestCaseData(new[] {1}).SetName("1 element"),
+                new TestCaseData(new[] {1, 2}).SetName("2 elements"),
+                new TestCaseData(new[] {1, 2, 3}).SetName("3 elements"),
+                new TestCaseData(new[] {1, 2, 3, 4}).SetName("4 elements"),
+                new TestCaseData(new[] {1, 2, 3, 4, 5}).SetName("5 elements")
+            };
+        }
+
+        [TestCaseSource("IntEnumerables")]
+        public void PermutationsReturnsCorrectNumberOfEnumerables(IEnumerable<int> enumerable)
+        {
+            var enumeratedArray = enumerable as int[] ?? enumerable.ToArray();
+
+            Assert.That(enumeratedArray.Permutations().Count(), 
+                Is.EqualTo(Factorial(enumeratedArray.Count())));
+        }
+
+        private static int Factorial(int x)
+        {
+            return x <= 1 ? 1 : x * Factorial(x - 1);
+        }
+    }
+}
